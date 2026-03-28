4e18ee12f7db31536973561d4f74f305f47c2c06
MG1005-lab <manikghodke16@gmail.com>
2026-03-28 20:15:26 +0530

    Update BC.java

--- POM/src/main/java/Base/BC.java
+++ POM/src/main/java/Base/BC.java
@@ -5,6 +5,8 @@ import java.time.Duration;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
